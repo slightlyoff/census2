f6a896f9b2e097b1e3cc41698d54fd54c137e4d4
slightlyoff <slightlyoff@f43fe37c-b329-11dd-9d88-f59b771712dd>
2008-11-16 02:38:37 +0000

    let run_census.sh take jetty-runner options at the command line

--- run_census.sh
+++ run_census.sh
@@ -4,4 +4,4 @@
 # cp -f srv/webdefault.xml srv/jetty/etc/webdefault.xml
 # (cd srv/jetty && java -jar start.jar)
 
-java -server -jar srv/jetty-runner-7.0.0.pre5.jar src/c2
+java -server -jar srv/jetty-runner-7.0.0.pre5.jar $@ src/c2
